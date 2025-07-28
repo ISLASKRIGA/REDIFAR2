f5276e658c1ebe02fa2a48bab23fd7723ca515ff
ISLASKRIGA <abrahamuvul@gmail.com>
2025-07-27 18:31:04 -0600

    update

--- src/components/ui/animated-hero.tsx
+++ src/components/ui/animated-hero.tsx
@@ -59,7 +59,7 @@ function Hero({ onLoginClick }: HeroProps) {
           </div>
           <div className="flex gap-4 flex-col">
             <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
-              <span className="text-black-600">La Red Hospitalaria es</span>
+              <span className="text-black-600">La Red Hospitalaria es demasiado</span>
               <span className="relative flex w-full justify-center overflow-hidden text-center md:pb-4 md:pt-1">
                 &nbsp;
                 {titles.map((title, index) => (
