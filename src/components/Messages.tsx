c67c948e65daf3b184f100656d0dc03bfe0c9b6d
Abraham <50091405+ISLASKRIGA@users.noreply.github.com>
2025-07-29 17:36:55 -0600

    Updated Messages.tsx

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -413,7 +413,7 @@ tempLastMessagesMap[partnerId] = lastMessage.content;
         <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
       </div>
     );
-  }
+  }d
   // Diccionario con el último mensaje por hospital
 const tempLastMessagesMap: { [hospitalId: string]: string } = {};
 
